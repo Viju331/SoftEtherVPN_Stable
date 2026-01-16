f2881ff0f1cb4b4a50f1f1d6ba27c1ae91f252df
Vijay Mali <vijumali331@gmail.com>
2026-01-16 12:47:23 +0530

    solved build error

--- qt-gui/src/VpnBackend.cpp
+++ qt-gui/src/VpnBackend.cpp
@@ -1,50 +1,60 @@
 #include "VpnBackend.h"
-// Include C headers for SoftEther backend
-extern "C" {
-#include "../../src/Cedar/CM.h"
-}
+// TODO: Include C headers for SoftEther backend when macOS backend is ready
+// For now, this is a placeholder implementation for the Qt GUI to build
+// extern "C" {
+// #include "../../src/Cedar/CM.h"
+// }
 
-VpnBackend::VpnBackend(QObject *parent) : QObject(parent) {
+VpnBackend::VpnBackend(QObject *parent) : QObject(parent)
+{
     // Optionally initialize backend here
 }
 
-bool VpnBackend::initialize() {
+bool VpnBackend::initialize()
+{
     // TODO: Initialize SoftEther client manager (CM)
     // Example: CM_Initialize();
     return true;
 }
 
-bool VpnBackend::addAccount(const QVariantMap &account) {
+bool VpnBackend::addAccount(const QVariantMap &account)
+{
     // TODO: Map QVariantMap to CM_ACCOUNT and call CM_AddAccount
     return true;
 }
 
-bool VpnBackend::editAccount(const QVariantMap &account) {
+bool VpnBackend::editAccount(const QVariantMap &account)
+{
     // TODO: Map QVariantMap to CM_ACCOUNT and call CM_EditAccount
     return true;
 }
 
-bool VpnBackend::deleteAccount(const QString &accountName) {
+bool VpnBackend::deleteAccount(const QString &accountName)
+{
     // TODO: Call CM_DeleteAccount
     return true;
 }
 
-QVariantList VpnBackend::listAccounts() const {
+QVariantList VpnBackend::listAccounts() const
+{
     // TODO: Call CM_EnumAccount and convert to QVariantList
     return QVariantList();
 }
 
-bool VpnBackend::setVLANs(const QVariantList &vlans) {
+bool VpnBackend::setVLANs(const QVariantList &vlans)
+{
     // TODO: Map QVariantList to CM_VLAN and call CM_SetVLANs
     return true;
 }
 
-QVariantList VpnBackend::getVLANs() const {
+QVariantList VpnBackend::getVLANs() const
+{
     // TODO: Call CM_GetVLANs and convert to QVariantList
     return QVariantList();
 }
 
-QVariantMap VpnBackend::getStatus(const QString &accountName) const {
+QVariantMap VpnBackend::getStatus(const QString &accountName) const
+{
     // TODO: Call CM_GetAccountStatus and convert to QVariantMap
     return QVariantMap();
 }
